495c278e4cc1a24d1f3225e861d170ab275237ba
aodfvnjfdt <110316152+aodfvnjfdt@users.noreply.github.com>
2023-01-26 22:28:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 默认各参数值，请自行修改.(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 PORT=${PORT:-'8080'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'88267842-9f15-423b-bba7-90773cf4f3ff'}
 WSPATH=${WSPATH:-'argo'}
 
 # 生成 Xray 配置文件
@@ -90,7 +90,7 @@ cat > config.json << EOF
                 }
             },
             "sniffing":{
-                "enabled":true,
+                "enabled":false
                 "destOverride":[
                     "http",
                     "tls",
